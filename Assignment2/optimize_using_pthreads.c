ba49609cdae9ca3d6b9b0e2b54968216521a84d4
Jeffrey Lau <jl4373@drexel.edu>
2025-02-06 13:47:57 -0500

    added assignment folders

---
+++ Assignment2/optimize_using_pthreads.c
@@ -0,0 +1,20 @@
+/* Implementation of PSO using pthreads.
+ *
+ * Author: Naga Kandasamy
+ * Date: January 31, 2025
+ *
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#include <string.h>
+#include "pso.h"
+
+int optimize_using_pthreads(char *function, int dim, int swarm_size, 
+                            float xmin, float xmax, int num_iter, int num_threads)
+{
+    int g = -1;
+
+    return g;
+}
