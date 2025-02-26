b90a675e228e1f68976f6df7c4f86ed529146094
jeffreylau1 <jeffl@JeffLaptop>
2025-02-26 12:43:09 -0500

    added assignment4

---
+++ Assignment4/blur_filter_kernel.cu
@@ -0,0 +1,13 @@
+/* Blur filter. Device code. */
+
+#ifndef _BLUR_FILTER_KERNEL_H_
+#define _BLUR_FILTER_KERNEL_H_
+
+#include "blur_filter.h"
+
+__global__ void blur_filter_kernel (const float *in, float *out, int size)
+{
+    return;
+}
+
+#endif /* _BLUR_FILTER_KERNEL_H_ */
