968f5b2ef2d305dc3131410898fbb78aa0a1955e
Sotero Casiano <91989167+soterocasiano@users.noreply.github.com>
2025-03-06 21:55:06 -0500

    Update trap_gold.cpp

--- Assignment5/trap_gold.cpp
+++ Assignment5/trap_gold.cpp
@@ -5,26 +5,26 @@
 extern "C" double compute_gold(float, float, int, float);
 
 /*------------------------------------------------------------------
- * Function:    f
- * Purpose:     Compute value of function to be integrated
- * Input args:  x
- * Output: sqrt((1 + x*x)/(1 + x*x*x*x)) 
- */
+ *  * Function:    f
+ *   * Purpose:     Compute value of function to be integrated
+ *    * Input args:  x
+ *     * Output: sqrt((1 + x*x)/(1 + x*x*x*x))
+ *      */
 
-float f(float x) 
+float f(float x)
 {
     return sqrtf((1 + x*x)/(1 + x*x*x*x));
-}  
+}
 
 /*------------------------------------------------------------------
- * Function:    Trap
- * Purpose:     Estimate integral from a to b of f using trap rule and
- *              n trapezoids
- * Input args:  a, b, n, h
- * Return val:  Estimate of the integral 
- */
+ *  * Function:    Trap
+ *   * Purpose:     Estimate integral from a to b of f using trap rule and
+ *    *              n trapezoids
+ *     * Input args:  a, b, n, h
+ *      * Return val:  Estimate of the integral
+ *       */
 
-double compute_gold(float a, float b, int n, float h) 
+double compute_gold(float a, float b, int n, float h)
 {
    double integral;
    int k;
@@ -36,6 +36,4 @@ double compute_gold(float a, float b, int n, float h)
 
    integral = integral*h;
    return integral;
-}  
-
-
+}
